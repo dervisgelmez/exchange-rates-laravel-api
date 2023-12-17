7bc9d5dcf5d389f2f617ea2a3425906213e1cded
Gelmez <gelmezdervis@gmail.com>
2023-12-17 22:53:53 +0300

    update - user request log controller

--- app/Http/Controllers/Api/UserRequestLogController.php
+++ app/Http/Controllers/Api/UserRequestLogController.php
@@ -3,7 +3,37 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\ApiBaseController;
+use App\Models\UserRequestLog;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 final class UserRequestLogController extends ApiBaseController
 {
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function index(Request $request): JsonResponse
+    {
+        $requestLogs = UserRequestLog::query();
+
+        return $this->successResponse(
+            $requestLogs->paginate($request->query->get('count', 15))->toArray()
+        );
+
+    }
+
+    /**
+     * @param int $userId
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getLogsByUser(int $userId, Request $request): JsonResponse
+    {
+        $requestLogs = UserRequestLog::query()->where('user_id', '=', $userId);
+
+        return $this->successResponse(
+            $requestLogs->paginate($request->query->get('count', 15))->toArray()
+        );
+    }
 }
